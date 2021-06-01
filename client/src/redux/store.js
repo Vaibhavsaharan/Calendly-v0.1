3ed0f122d5e18c38cc090fe758743da9e6699a22
Vaibhav Saharan <vaibhavsaharans@gmail.com>
2021-06-01 14:55:33 +0530

    mid Deploy

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' 
 import thunk from 'redux-thunk';
@@ -16,7 +15,7 @@ const middleware = [thunk];
 export const store = createStore(
   persistedReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 export const persistor = persistStore(store)
 
